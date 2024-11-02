81ea40736f0af5bd3ed843d465595104f4ed2847
chulin wang <chulin.wang@thoughtworks.com>
2024-11-02 18:48:24 +0800

    feat: show pic in text list

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ a simpleList to show text and pic for exercise
 ### Tasking
 - [x] show text in screen
 - [x] show text list in screen
-- [ ] show pic in text list
+- [x] show pic in text list
   - [x] show a pic in list item
+  - [ ] show title,description in list item
+  
--- app/src/main/java/com/example/simplelist/MainActivity.kt
+++ app/src/main/java/com/example/simplelist/MainActivity.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.simplelist.ui.theme.SimpleListTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
             SimpleListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     List(
-                         modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -34,15 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun List(modifier: Modifier = Modifier) {
-    Column {
-        ListItem("1.This is text1.")
-        ListItem("2.This is text2.")
-        ListItem("3.This is text3.")
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        items(5) { index ->
+            ListItem("${index + 1}. This is text ${index + 1}.")
+        }
     }
 }
 
 @Composable
-fun ListItem(title:String,modifier: Modifier = Modifier) {
+fun ListItem(title: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
     Column {
         Text(
@@ -51,7 +55,10 @@ fun ListItem(title:String,modifier: Modifier = Modifier) {
         Image(
             painter = image,
             contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(200.dp)
         )
+
     }
 }
 
