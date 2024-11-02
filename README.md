ced1335bee25a71f975499d5416f4096fdefd47e
chulin wang <chulin.wang@thoughtworks.com>
2024-11-02 18:30:22 +0800

    feat: show a pic in list item

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ a simpleList to show text and pic for exercise
 
 ### Tasking
 - [x] show text in screen
-  - [x] adjust text fontSize
 - [x] show text list in screen
-- [ ] show pic in screen
+- [ ] show pic in text list
+  - [x] show a pic in list item
--- app/src/main/java/com/example/simplelist/MainActivity.kt
+++ app/src/main/java/com/example/simplelist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,8 +12,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.simplelist.ui.theme.SimpleListTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +23,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreetingText(
-                        message = "Android", modifier = Modifier.padding(innerPadding)
+                    List(
+                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -32,22 +33,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun List(modifier: Modifier = Modifier) {
+    Column {
+        ListItem("1.This is text1.")
+        ListItem("2.This is text2.")
+        ListItem("3.This is text3.")
+    }
+}
+
+@Composable
+fun ListItem(title:String,modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
     Column {
         Text(
-            text = message,
-            fontSize = 40.sp,
-            lineHeight = 116.sp
+            text = title
         )
-        Text(
-            text = "2.This is a Text.",
-            fontSize = 40.sp,
-            lineHeight = 116.sp
-        )
-        Text(
-            text = "3.This is a Text.",
-            fontSize = 40.sp,
-            lineHeight = 116.sp
+        Image(
+            painter = image,
+            contentDescription = null,
         )
     }
 }
@@ -57,7 +60,6 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun TextPreview() {
     SimpleListTheme {
-        GreetingText("1.This is a Text.")
-
+        List()
     }
 }
