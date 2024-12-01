b8b34e524dc374b1cab21930adffa45dc3b1e34e
chulin-tw <chulin.wang@thoughtworks.com>
2024-12-01 19:16:00 +0800

    feat: add MomentsHeader Preview

--- app/src/main/java/com/example/moments/ui/momentsheader/MomentsHeader.kt
+++ app/src/main/java/com/example/moments/ui/momentsheader/MomentsHeader.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.moments.R
@@ -72,4 +73,10 @@ fun MomentsHeader(modifier: Modifier = Modifier) {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewMomentsHeader(){
+    MomentsHeader()
 }
