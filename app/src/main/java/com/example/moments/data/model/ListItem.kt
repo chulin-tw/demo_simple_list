9179f032b52e54504e4e4ef30fa76c3d9ac96521
chulin-tw <chulin.wang@thoughtworks.com>
2024-12-01 12:13:06 +0800

    refactor: remove unuseful annotation

--- app/src/main/java/com/example/moments/data/model/ListItem.kt
+++ app/src/main/java/com/example/moments/data/model/ListItem.kt
@@ -1,21 +1,15 @@
 package com.example.moments.data.model
 
-import kotlinx.serialization.Serializable
-
-
-@Serializable
 data class ListItem(
     val userInfo: UserInfo,
     val momentInfo: MomentInfo
 )
 
-@Serializable
 data class UserInfo(
     val username: String,
     val avatar: String
 )
 
-@Serializable
 data class MomentInfo(
     val text: String,
     val picture: List<String>?
