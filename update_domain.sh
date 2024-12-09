4a776becdbdfb289d1173461cc42ccaf2d51419f
chulin-tw <chulin.wang@thoughtworks.com>
2024-12-09 22:47:46 +0800

    chore: update domain.sh

--- update_domain.sh
+++ update_domain.sh
@@ -7,9 +7,10 @@ if [ -z "$current_ip" ]; then
   exit 1
 fi
 
-project_dir="/Users/chulinwang/AndroidStudioProjects/Moments"
+project_dir_kotlin="/Users/chulinwang/AndroidStudioProjects/Moments"
+project_dir_java="/Users/chulinwang/AndroidStudioProjects/Moments/MomentsJava"
 
-server_js_path="$project_dir/backend/server.js"
+server_js_path="$project_dir_kotlin/backend/server.js"
 if [ -f "$server_js_path" ]; then
   sed -i '' "s/^const domain = .*$/const domain = '$current_ip';/" "$server_js_path"
   echo "已成功修改 $server_js_path"
@@ -18,12 +19,21 @@ else
   exit 1
 fi
 
-list_api_module_path="$project_dir/app/src/main/java/com/example/moments/data/api/di/ListApiModule.kt"
+list_api_module_path="$project_dir_kotlin/app/src/main/java/com/example/moments/data/api/di/ListApiModule.kt"
 if [ -f "$list_api_module_path" ]; then
   sed -i '' "s/^private const val DOMAIN_URL = \".*\"$/private const val DOMAIN_URL = \"$current_ip\"/" "$list_api_module_path"
   echo "已成功修改 $list_api_module_path"
 else
   echo "找不到文件 $list_api_module_path"
+  exit 1
+fi
+
+list_api_module_path_java="$project_dir_java/app/src/main/java/com/example/momentsjava/data/api/di/ListApiModule.java"
+if [ -f "$list_api_module_path_java" ]; then
+  sed -i '' "s/^private static final String DOMAIN_URL = \".*\"$/private static final String DOMAIN_URL = \"$current_ip\"/" "$list_api_module_path_java"
+  echo "已成功修改 $list_api_module_path_java"
+else
+  echo "找不到文件 $list_api_module_path_java"
   exit 1
 fi
 
