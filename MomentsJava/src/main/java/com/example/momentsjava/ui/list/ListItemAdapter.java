29ee3b488b4eae62eec9704e6baaa2e8d1b7fc5c
chulin-tw <chulin.wang@thoughtworks.com>
2025-01-02 20:55:53 +0800

    refactor: move methods from ListItemAdapter to ListItemViewHolder

--- MomentsJava/src/main/java/com/example/momentsjava/ui/list/ListItemAdapter.java
+++ MomentsJava/src/main/java/com/example/momentsjava/ui/list/ListItemAdapter.java
@@ -2,18 +2,11 @@ package com.example.momentsjava.ui.list;
 
 import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
-import com.example.momentsjava.R;
 import com.example.momentsjava.data.model.ListItem;
 import com.example.momentsjava.databinding.ListItemViewBinding;
 
@@ -38,75 +31,8 @@ public class ListItemAdapter extends RecyclerView.Adapter<ListItemViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ListItemViewHolder holder, int position) {
         ListItem listItem = listItems.get(position);
-        holder.binding.setListItem(listItem);
-        holder.binding.executePendingBindings();
-        Glide.with(holder.itemView)
-                .load(listItem.getUserInfo().getAvatar())
-                .transform(new RoundedCorners(12))
-                .into(holder.binding.userAvatar);
-        setPictures(listItem.getMomentInfo().getPicture(), holder.binding.pictureContainer, holder.itemView);
-        setExpandableContent(holder);
+        holder.bind(listItem);
     }
-
-    private static void setExpandableContent(@NonNull ListItemViewHolder holder) {
-        holder.binding.content.post(() -> {
-            boolean isContentExpandable = holder.binding.content.getLineCount() > 5;
-            holder.binding.content.setMaxLines(isContentExpandable ? 5 : Integer.MAX_VALUE);
-            holder.binding.expandToggle.setVisibility(isContentExpandable ? View.VISIBLE : View.GONE);
-        });
-        holder.binding.expandToggle.setOnClickListener(v -> {
-            String expandText = holder.itemView.getContext().getString(R.string.expandText);
-            String foldText = holder.itemView.getContext().getString(R.string.foldText);
-            boolean isExpanded = holder.binding.expandToggle.getText().equals(expandText);
-            holder.binding.content.setMaxLines(isExpanded ? Integer.MAX_VALUE : 5);
-            holder.binding.expandToggle.setText(isExpanded ? foldText : expandText);
-        });
-    }
-
-    private void setPictures(List<String> pictures, LinearLayout picturesContainer, View itemView) {
-        picturesContainer.removeAllViews();
-
-        int rows = (int) Math.ceil(pictures.size() / 3.0);
-        for (int row = 0; row < rows; row++) {
-            LinearLayout rowLayout = createRowLayout(itemView);
-            addPicturesToRow(pictures, row, rowLayout, itemView);
-            picturesContainer.addView(rowLayout);
-        }
-    }
-
-    private LinearLayout createRowLayout(View itemView) {
-        LinearLayout rowLayout = new LinearLayout(itemView.getContext());
-        rowLayout.setOrientation(LinearLayout.HORIZONTAL);
-        rowLayout.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        return rowLayout;
-    }
-
-    private void addPicturesToRow(List<String> pictures, int row, LinearLayout rowLayout, View itemView) {
-        int startIndex = row * 3;
-        int endIndex = Math.min(startIndex + 3, pictures.size());
-        for (int i = startIndex; i < endIndex; i++) {
-            ImageView pictureView = createPictureView(pictures.get(i), itemView);
-            rowLayout.addView(pictureView);
-        }
-    }
-
-    private ImageView createPictureView(String pictureUrl, View itemView) {
-        ImageView pictureView = new ImageView(itemView.getContext());
-        Glide.with(itemView)
-                .load(pictureUrl)
-                .apply(new RequestOptions().centerCrop())
-                .into(pictureView);
-
-        int width = (int) (110 * itemView.getContext().getResources().getDisplayMetrics().density);
-        int height = (int) (110 * itemView.getContext().getResources().getDisplayMetrics().density);
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, height);
-        params.setMargins(0, 0, 8, 8);
-        pictureView.setLayoutParams(params);
-        return pictureView;
-    }
-
 
     @Override
     public int getItemCount() {
--- MomentsJava/src/main/java/com/example/momentsjava/ui/list/ListItemViewHolder.java
+++ MomentsJava/src/main/java/com/example/momentsjava/ui/list/ListItemViewHolder.java
@@ -1,15 +1,96 @@
 package com.example.momentsjava.ui.list;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.momentsjava.R;
+import com.example.momentsjava.data.model.ListItem;
 import com.example.momentsjava.databinding.ListItemViewBinding;
 
+import java.util.List;
+
 public class ListItemViewHolder extends RecyclerView.ViewHolder {
-    final ListItemViewBinding binding;
+    private final ListItemViewBinding binding;
 
     public ListItemViewHolder(@NonNull ListItemViewBinding binding) {
         super(binding.getRoot());
         this.binding = binding;
+    }
+
+    public void bind(ListItem listItem) {
+        binding.setListItem(listItem);
+        binding.executePendingBindings();
+        Glide.with(itemView)
+                .load(listItem.getUserInfo().getAvatar())
+                .transform(new RoundedCorners(12))
+                .into(binding.userAvatar);
+        setPictures(listItem.getMomentInfo().getPicture(), binding.pictureContainer, itemView);
+        setExpandableContent();
+    }
+
+    private void setExpandableContent() {
+        binding.content.post(() -> {
+            boolean isContentExpandable = binding.content.getLineCount() > 5;
+            binding.content.setMaxLines(isContentExpandable ? 5 : Integer.MAX_VALUE);
+            binding.expandToggle.setVisibility(isContentExpandable ? View.VISIBLE : View.GONE);
+        });
+        binding.expandToggle.setOnClickListener(v -> {
+            String expandText = itemView.getContext().getString(R.string.expandText);
+            String foldText = itemView.getContext().getString(R.string.foldText);
+            boolean isExpanded = binding.expandToggle.getText().equals(expandText);
+            binding.content.setMaxLines(isExpanded ? Integer.MAX_VALUE : 5);
+            binding.expandToggle.setText(isExpanded ? foldText : expandText);
+        });
+    }
+
+    private void setPictures(List<String> pictures, LinearLayout picturesContainer, View itemView) {
+        picturesContainer.removeAllViews();
+
+        int rows = (int) Math.ceil(pictures.size() / 3.0);
+        for (int row = 0; row < rows; row++) {
+            LinearLayout rowLayout = createRowLayout(itemView);
+            addPicturesToRow(pictures, row, rowLayout, itemView);
+            picturesContainer.addView(rowLayout);
+        }
+    }
+
+    private LinearLayout createRowLayout(View itemView) {
+        LinearLayout rowLayout = new LinearLayout(itemView.getContext());
+        rowLayout.setOrientation(LinearLayout.HORIZONTAL);
+        rowLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        return rowLayout;
+    }
+
+    private void addPicturesToRow(List<String> pictures, int row, LinearLayout rowLayout, View itemView) {
+        int startIndex = row * 3;
+        int endIndex = Math.min(startIndex + 3, pictures.size());
+        for (int i = startIndex; i < endIndex; i++) {
+            ImageView pictureView = createPictureView(pictures.get(i), itemView);
+            rowLayout.addView(pictureView);
+        }
+    }
+
+    private ImageView createPictureView(String pictureUrl, View itemView) {
+        ImageView pictureView = new ImageView(itemView.getContext());
+        Glide.with(itemView)
+                .load(pictureUrl)
+                .apply(new RequestOptions().centerCrop())
+                .into(pictureView);
+
+        int width = (int) (110 * itemView.getContext().getResources().getDisplayMetrics().density);
+        int height = (int) (110 * itemView.getContext().getResources().getDisplayMetrics().density);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, height);
+        params.setMargins(0, 0, 8, 8);
+        pictureView.setLayoutParams(params);
+        return pictureView;
     }
 }
