c4ff37184687f41a506eace5f3d7b7db45f87a69
chulin-tw <chulin.wang@thoughtworks.com>
2025-01-07 14:56:04 +0800

    test: add mockContext in ListDataSourceTest

--- MomentsKotlin/src/test/java/com/example/moments/data/datasource/ListDataSourceTest.kt
+++ MomentsKotlin/src/test/java/com/example/moments/data/datasource/ListDataSourceTest.kt
@@ -1,22 +1,30 @@
 package com.example.moments.data.datasource
 
-import com.example.moments.momentItems
+import android.content.Context
 import com.example.moments.data.api.ListApiService
+import com.example.moments.momentItems
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import java.io.File
 
 class ListDataSourceTest {
     private lateinit var mockListApiService: ListApiService
     private lateinit var listDataSource: ListDataSource
+    private lateinit var mockContext: Context
 
     @Before
     fun setUp() {
         mockListApiService = mockk()
-        listDataSource = ListDataSource(mockListApiService)
+        mockContext = mockk()
+        every { mockContext.applicationContext } returns mockContext
+        every { mockContext.getString(any()) } returns "Mocked String"
+        every { mockContext.filesDir } returns File("mocked_files_dir")
+        listDataSource = ListDataSource(mockListApiService, mockContext)
     }
 
     @Test
