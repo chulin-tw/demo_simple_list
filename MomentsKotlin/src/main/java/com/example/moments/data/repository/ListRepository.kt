2122cc23ff6ed1b21437c667f56e02b81ddb331c
chulin-tw <chulin.wang@thoughtworks.com>
2025-03-06 10:35:35 +0800

    feat: add thread pool in repository

--- MomentsKotlin/src/main/java/com/example/moments/data/repository/ListRepository.kt
+++ MomentsKotlin/src/main/java/com/example/moments/data/repository/ListRepository.kt
@@ -2,9 +2,22 @@ package com.example.moments.data.repository
 
 import com.example.moments.data.datasource.ListDataSource
 import com.example.moments.data.model.ListItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class ListRepository(private val listDataSource: ListDataSource) {
-     suspend fun getList(): List<ListItem> {
-        return listDataSource.fetchList()
+    private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
+    suspend fun getList(): List<ListItem> {
+        return withContext(Dispatchers.IO) {
+            val res = executorService.submit<List<ListItem>> {
+                runBlocking {
+                    listDataSource.fetchList()
+                }
+            }
+            res.get()
+        }
     }
 }
